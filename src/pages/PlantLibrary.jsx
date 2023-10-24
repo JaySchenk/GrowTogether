5d806a1cdad739a6de5ed2362ed2a96805a6b470
liubovpo <lyubov.html@gmail.com>
2023-10-24 15:31:16 +0200

    .map render cards

--- src/pages/PlantLibrary.jsx
+++ src/pages/PlantLibrary.jsx
@@ -1,21 +1,32 @@
-import { Box, Text } from "@mantine/core";
+import { useState, useEffect } from "react";
 import NavbarMobile from "../components/NavbarMobile";
+import PlantCard from "../components/PlantCard";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 const PlantLibrary = () => {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/api/plantcare`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {setPlants(data)})
+
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "calc(100vh - 100px)",
-      }}
-    >
-      <Text size="xl" weight="bold" align="center">
-        library
-      </Text>
+    <div className="plant-library">
+      {plants.map((plant, index) => (
+        <PlantCard key={index} plant={plant} />
+      ))}
       <NavbarMobile/>
-    </Box>
+    </div>
   );
 };
 
