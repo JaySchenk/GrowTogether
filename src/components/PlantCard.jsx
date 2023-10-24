5643d7a437ed5b4c6cb1ed1b604dbfdec53c2f95
liubovpo <lyubov.html@gmail.com>
2023-10-24 15:13:35 +0200

    PlantCard component

---
+++ src/components/PlantCard.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@mantine/core";
+import { Link } from "react-router-dom";
+
+const PlantCard = ({plant}) => {
+  return (
+    <div className="plant-card">
+      <img src={plant.image} alt={plant.species} />
+      <p>{plant.species}</p>
+      <p>Difficulty Level: {plant.care_instructions.difficulty_care_level}</p>
+      <Link to={`/plantcare/${plant._id}`}> <Button> info </Button></Link>
+    </div>
+  );
+};
+
+export default PlantCard;
