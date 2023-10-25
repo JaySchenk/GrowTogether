d9dbf5eb3e8269fef35a57dbaf08bffbaa65445a
liubovpo <lyubov.html@gmail.com>
2023-10-25 21:25:38 +0200

    mobile navbar hide on wide screens

--- src/components/NavbarMobile.jsx
+++ src/components/NavbarMobile.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const NavbarMobile = () => {
   return (
-    <div className="navbar-mobile">
+    <div className="navbar-mobile sm:hidden">
       <Link to="/uplant">Home</Link>
       <Link to="/plantcare">Library</Link>
       <Link to="/uprofile">Profile</Link>
