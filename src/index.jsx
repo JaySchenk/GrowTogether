b1a7e1b8022e45fd6179255c67bc4d1db460bd99
JanJaap <j-jschenk@hotmail.com>
2023-10-30 09:49:54 +0100

    disabled dist/output.css

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import '../dist/output.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+// import '../dist/output.css';
 // import './input.css';
 import SessionContextProvider, {
   SessionContext,
-} from './contexts/SessionContext';
+} from "./contexts/SessionContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <SessionContextProvider>
-          <App />
+        <App />
       </SessionContextProvider>
     </BrowserRouter>
   </React.StrictMode>
