bb041f7cb378337e7f58d52f70b52303eee85cca
Kerem Karadag <akaradag32@gmail.com>
2023-10-30 09:15:42 +0100

    button component fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import { Link, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import UserPlantPage from './pages/UserPlantPage';
-import UserProfilePage from './pages/UserProfilePage';
-import PlantCarePage from './pages/PlantCarePage';
-import CreatePlant from './pages/CreatePlant';
-import PlantLibrary from './pages/PlantLibrary';
-import PrivateRoute from './components/privateRoute';
-import { SessionContext } from './contexts/SessionContext';
-import { useContext, useEffect } from 'react';
-import LoggedInNavbar from './components/LoggedInNavbar';
-import LoggedOutNavbar from './components/LoggedOutNavbar';
-import Button from './components/Button';
-import { AiFillHome } from 'react-icons/ai';
-=======
 import { Link, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -30,7 +12,8 @@ import { SessionContext } from "./contexts/SessionContext";
 import { useContext, useEffect } from "react";
 import LoggedInNavbar from "./components/LoggedInNavbar";
 import LoggedOutNavbar from "./components/LoggedOutNavbar";
->>>>>>> 1b32ddee590b370cbbbbdb5f120286abff1de826
+import Button from "./components/Button";
+import { AiFillHome } from "react-icons/ai";
 
 function App() {
   const { isAuthenticated, isLoading } = useContext(SessionContext);
@@ -38,40 +21,31 @@ function App() {
 
   return (
     <div>
-      <header className=' sticky top-0 p-8 flex justify-between items-center h-16'>
-        <Link to='/'>
-          <Button name={'Home Page'}>
+      <header className=" sticky top-0 p-8 flex justify-between items-center h-16">
+        <Link to="/">
+          <Button name={"Home Page"}>
             <AiFillHome />
           </Button>
         </Link>
-<<<<<<< HEAD
-        <div className='w-2/3 flex justify-end'>
-=======
         <div className="w-2/3 flex justify-end">
->>>>>>> 1b32ddee590b370cbbbbdb5f120286abff1de826
           {isAuthenticated && !isLoading ? (
             <LoggedInNavbar />
           ) : (
             <LoggedOutNavbar />
           )}
-<<<<<<< HEAD
-          <Link to='/plantcare' className=' hover:underline ml-4'>
-            <Button name={'Plant Care Library'}></Button>
-=======
-          <Link to="/plantcare" className=" hover:underline ml-6">
-            Plant Care Library
->>>>>>> 1b32ddee590b370cbbbbdb5f120286abff1de826
+          <Link to="/plantcare" className=" hover:underline ml-4">
+            <Button name={"Plant Care Library"}></Button>
           </Link>
         </div>
       </header>
 
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/plantcare' element={<PlantLibrary />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/plantcare" element={<PlantLibrary />} />
         <Route
-          path='/uplant'
+          path="/uplant"
           element={
             <PrivateRoute>
               <UserPlantPage />
@@ -79,16 +53,16 @@ function App() {
           }
         />
         <Route
-          path='/uprofile'
+          path="/uprofile"
           element={
             <PrivateRoute>
               <UserProfilePage />
             </PrivateRoute>
           }
         />
-        <Route path='/plantcare/:plantCareId' element={<PlantCarePage />} />
+        <Route path="/plantcare/:plantCareId" element={<PlantCarePage />} />
         <Route
-          path='/createplant'
+          path="/createplant"
           element={
             <PrivateRoute>
               <CreatePlant />
--- src/components/LoggedInNavbar.jsx
+++ src/components/LoggedInNavbar.jsx
@@ -1,33 +1,30 @@
-import React, { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { SessionContext } from '../contexts/SessionContext';
-import Button from './Button';
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { SessionContext } from "../contexts/SessionContext";
+import Button from "./Button";
 
 const LoggedInNavbar = () => {
   const { setIsAuthenticated } = useContext(SessionContext);
   const navigate = useNavigate();
 
   function handleClick() {
-    localStorage.removeItem('authToken');
+    localStorage.removeItem("authToken");
     setIsAuthenticated(false);
-    navigate('/login');
+    navigate("/login");
   }
 
   return (
-    <div className='flex'>
-      <Button name={'Log Out'} onClick={handleClick}></Button>
-      <Link to='/uplant'>
-        <Button name={'User Plants'}></Button>
+    <div className="flex">
+      <Button name={"Log Out"} onClick={handleClick}></Button>
+      <Link to="/uplant">
+        <Button name={"User Plants"}></Button>
       </Link>
-      <Link to='/uprofile'>
-        <Button name={'User Profile'}></Button>
+      <Link to="/uprofile">
+        <Button name={"User Profile"}></Button>
       </Link>
-<<<<<<< HEAD
-      <Link to='/createplant'>
-        <Button name={'Create Plant'}></Button>
+      <Link to="/createplant">
+        <Button name={"Create Plant"}></Button>
       </Link>
-=======
->>>>>>> 1b32ddee590b370cbbbbdb5f120286abff1de826
     </div>
   );
 };
