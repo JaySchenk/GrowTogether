b2b7e3d08dfcbd3f7eb4208b11c06d5968a0c8ad
JanJaap <j-jschenk@hotmail.com>
2023-10-31 16:07:59 +0100

    check if plant needs water

--- src/components/PlantUserCard.jsx
+++ src/components/PlantUserCard.jsx
@@ -2,12 +2,40 @@ import { Link } from "react-router-dom";
 import { useState, useEffect, useContext } from "react";
 import { SessionContext } from "../contexts/SessionContext";
 import { ChevronDoubleRightIcon } from "@heroicons/react/24/outline";
+import NewUserPlant from "./NewUserPlant";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 const PlantUserCard = ({ plant, fetchPlants }) => {
   const [plants, setPlants] = useState([]);
+  const [needWater, setNeedWater] = useState(false);
   const { userId } = useContext(SessionContext);
+  const [showNewUserPlant, setShowNewUserPlant] = useState(false);
+
+  const checkWateringStatus = () => {
+    if (
+      plant.careActivityDate &&
+      plant.careActivityDate.length > 0 &&
+      plants.length > 0
+    ) {
+      const currentPlant = plants.find((one) => one._id === plant.plantSpecies);
+      const waterFrequency = currentPlant.care_instructions.water.frequency;
+
+      if (waterFrequency) {
+        const frequencyDays = parseInt(waterFrequency.split(" ")[0]);
+        const lastCareDate = new Date(plant.careActivityDate[0].dateOfCare);
+        const currentDate = new Date();
+        const timeDifference = currentDate.getTime() - lastCareDate.getTime();
+        const daysDifference = Math.floor(
+          timeDifference / (1000 * 60 * 60 * 24)
+        );
+
+        if (daysDifference > frequencyDays) {
+          setNeedWater(true);
+        }
+      }
+    }
+  };
 
   useEffect(() => {
     fetch(`${API_URL}/api/plantcare`)
@@ -20,9 +48,14 @@ const PlantUserCard = ({ plant, fetchPlants }) => {
       .then((data) => {
         setPlants(data);
       })
-
       .catch((error) => console.log(error));
   }, []);
+
+  useEffect(() => {
+    if (plants.length > 0) {
+      checkWateringStatus();
+    }
+  }, [plants]);
 
   if (!plants.length) {
     return null;
@@ -53,45 +86,61 @@ const PlantUserCard = ({ plant, fetchPlants }) => {
   };
 
   return (
-    <div className='plant-card w-11/12 md:w-1/2 max-w-2xl duration-300'>
-      <div className='plant-info -ml-9'>
+    <div className="plant-card w-11/12 md:w-1/2 max-w-2xl duration-300">
+      <div className="plant-info -ml-9">
         {plant.plantPicture ? (
           <img
-            className='rounded-full sm:rounded-2xl h-1/5 duration-300'
+            className="rounded-full sm:rounded-2xl h-1/5 duration-300"
             src={plant.plantPicture}
             alt={plant.plantSpecies}
           />
         ) : (
           <img
-            className='rounded-full sm:rounded-2xl h-28 lg:h-36 duration-300'
-            src='https://ih1.redbubble.net/image.949338818.5434/aps,504x498,large,transparent-pad,600x600,f8f8f8.jpg'
+            className="rounded-full sm:rounded-2xl h-28 lg:h-36 duration-300"
+            src="https://ih1.redbubble.net/image.949338818.5434/aps,504x498,large,transparent-pad,600x600,f8f8f8.jpg"
             alt={plant.plantSpecies}
           />
         )}
-        <div className='text-left ml-4'>
-          <p className='font-semibold text-lg capitalize text-sky-900'>
+        <div className="text-left ml-4">
+          <p className="font-semibold text-lg capitalize text-sky-900">
             {plant.plantName}
           </p>
-          <p className='font-medium text-base text-gray-600'>
+          <p className="font-medium text-base text-gray-600">
             {currentPlant?.species}
           </p>
-          <button className='bg-emerald-600 text-white p-2 sm:p-3 px-3 sm:px-5 rounded-full self-center mt-4 sm:mt-10'>
+          <button className="bg-emerald-600 text-white p-2 sm:p-3 px-3 sm:px-5 rounded-full self-center mt-4 sm:mt-10">
             Edit
           </button>
           <button
             onClick={handleDelete}
-            className='bg-white text-emerald-600 p-2 sm:p-3 rounded-full self-center mt-2 sm:mt-10 ml-4 sm:ml-4'
+            className="bg-white text-emerald-600 p-2 sm:p-3 rounded-full self-center mt-2 sm:mt-10 ml-4 sm:ml-4"
           >
             Delete
           </button>
+          {showNewUserPlant ? (
+            <button
+              onClick={() => setShowNewUserPlant(false)}
+              className="bg-white text-red-600 p-2 sm:p-3 rounded-full self-center mt-2 sm:mt-10 ml-4 sm:ml-4"
+            >
+              Cancel
+            </button>
+          ) : (
+            <button
+              onClick={() => setShowNewUserPlant(true)}
+              className="bg-white text-emerald-600 p-2 sm:p-3 rounded-full self-center mt-2 sm:mt-10 ml-4 sm:ml-4"
+            >
+              Water plant
+            </button>
+          )}
         </div>
       </div>
-      <div className='plant-link chevron-double-right'>
+      <div className="plant-link chevron-double-right">
         <Link to={`/plantcare/${plant.plantSpecies}`}>
           {" "}
-          <ChevronDoubleRightIcon className='w-8 text-emerald-600' />{" "}
+          <ChevronDoubleRightIcon className="w-8 text-emerald-600" />{" "}
         </Link>
       </div>
+      {showNewUserPlant && <NewUserPlant type="newCare" plantId={plant._id} />}
     </div>
   );
 };
--- src/pages/PlantCarePage.jsx
+++ src/pages/PlantCarePage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import BackButton from "../components/BackButton";
 import NavbarMobile from "../components/NavbarMobile";
-import Light from "/light.png"
+import Light from "/light.png";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -14,44 +14,58 @@ const PlantCarePage = ({ match }) => {
     fetch(`${API_URL}/api/plantcare/${plantCareId}`)
       .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         return response.json();
       })
       .then((data) => {
-          setPlant(data.Plant);
-          console.log(data.Plant);})
+        setPlant(data.Plant);
+        console.log(data.Plant);
+      })
       .catch((error) => console.log(error));
   }, [plantCareId]);
 
   return (
-    
     <div>
       {plant ? (
         <div>
           <div className="plant-overview">
             {/* <BackButton />  */}
             <div>
-              <p className="font-medium text-4xl  text-sky-950">{plant.species}</p>
+              <p className="font-medium text-4xl  text-sky-950">
+                {plant.species}
+              </p>
               <div></div>
               <img src={Light} alt="" />
               <p>LIGHT</p>
               <p>35-40%</p>
               <p>Temperature: {plant.care_instructions.temperature}</p>
-              <p>Light Requirement: {plant.care_instructions.light_requirement}</p>
-              <p>Water: {plant.care_instructions.water.frequency}</p>
+              <p>
+                Light Requirement: {plant.care_instructions.light_requirement}
+              </p>
+              <p>
+                Water once every {plant.care_instructions.water.frequency} days
+              </p>
               <p>Soil Type: {plant.care_instructions.soil_type}</p>
             </div>
-            <img src={plant.image} alt={plant.species}/>
+            <img src={plant.image} alt={plant.species} />
           </div>
           <h2>Care Instructions:</h2>
           <p>About: {plant.about}</p>
           <p>Pot Size: {plant.care_instructions.pot_size}</p>
-          <p>Growth Stages: {plant.care_instructions.growth_stages.join(', ')}</p>
-          <p>Common Pests and Diseases: {plant.care_instructions.common_pests_diseases}</p>
+          <p>
+            Growth Stages: {plant.care_instructions.growth_stages.join(", ")}
+          </p>
+          <p>
+            Common Pests and Diseases:{" "}
+            {plant.care_instructions.common_pests_diseases}
+          </p>
           <p>Toxicity: {plant.care_instructions.toxicity}</p>
-          <p>Difficulty Care Level: {plant.care_instructions.difficulty_care_level}</p>
-          <NavbarMobile/>
+          <p>
+            Difficulty Care Level:{" "}
+            {plant.care_instructions.difficulty_care_level}
+          </p>
+          <NavbarMobile />
         </div>
       ) : (
         <p>Loading...</p>
