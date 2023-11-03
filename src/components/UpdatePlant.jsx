1f02e0acd12ddc7955a3c2198753611b9224bfa7
Kerem Karadag <akaradag32@gmail.com>
2023-11-03 13:07:04 +0100

    update picture feature

--- src/components/UpdatePlant.jsx
+++ src/components/UpdatePlant.jsx
@@ -14,32 +14,70 @@ const UpdatePlant = ({ plantId }) => {
   const [product, setProduct] = useState([]);
   const [activity, setActivity] = useState([]);
   const [reminderSettings, setReminderSettings] = useState(true);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const { userId } = useContext(SessionContext);
 
   const handleGoBack = () => {
     navigate("/uplant");
     return <Loader />;
   };
+  /////drag&drop///
+  function dropHandler(ev) {
+    console.log("File(s) dropped");
+    document.querySelector(".drag").classList.remove("dragndrop");
+    ev.preventDefault();
+    setPlantPicture(null);
+
+    if (ev.dataTransfer.items) {
+      [...ev.dataTransfer.items].forEach((item, i) => {
+        if (item.kind === "file") {
+          const file = item.getAsFile();
+          console.log(`… file[${i}].name = ${file.name}`);
+          setPlantPicture(file);
+        }
+      });
+    } else {
+      [...ev.dataTransfer.files].forEach((file, i) => {
+        console.log(`… file[${i}].name = ${file.name}`);
+        setPlantPicture(file);
+      });
+    }
+  }
+
+  function dragOverHandler(ev) {
+    console.log("File(s) in drop zone");
+    document.querySelector(".drag").classList.add("dragndrop");
+    ev.preventDefault();
+  }
+  ////////
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    if (isSubmitting) {
+      return;
+    }
+
+    setIsSubmitting(true);
     const payload = {
       plantName: plantName,
       plantSpecies: plantSpecies,
-      plantPicture,
       plantCutting: plantCutting,
       plantSize: plantSize,
       reminderSettings: reminderSettings,
     };
+    const form_data = new FormData();
+
+    form_data.append("Data", JSON.stringify(payload));
+    form_data.append("plantPicture", plantPicture);
+
+    console.log(form_data);
     try {
       const response = await fetch(
         `${import.meta.env.VITE_API_URL}/api/userplantsUpdate/${plantId}`,
         {
           method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(payload),
+          body: form_data,
         }
       );
 
@@ -87,66 +125,132 @@ const UpdatePlant = ({ plantId }) => {
   }, []);
 
   return (
-    <div className="m-auto max-w-md flex flex-col justify-center mt-20">
-      <h1 className="text-xl font-bold text-center">Update plant</h1>
-      {plantName ? (
-        <form className="flex flex-col gap-4 mt-8" onSubmit={handleSubmit}>
-          <p className="-mb-1 font-medium">Plant name</p>
+    <div className='m-auto max-w-md flex flex-col justify-center mt-20'>
+      <h1 className='text-xl font-bold text-center'>Update plant</h1>
+      {plantName && !isSubmitting ? (
+        <form className='flex flex-col gap-4 mt-8' onSubmit={handleSubmit}>
+          <p className='-mb-1 font-medium'>Plant name</p>
           <input
-            type="text"
+            type='text'
             value={plantName}
             onChange={(event) => setPlantName(event.target.value)}
             required
-            placeholder="Plant name"
-            className="p-2 border rounded-lg"
+            placeholder='Plant name'
+            className='p-2 border rounded-lg'
           />
-          <p className="-mb-1 font-medium">Species</p>
+          <p className='-mb-1 font-medium'>Species</p>
           <select
             value={plantSpecies}
             onChange={(event) => setPlantSpecies(event.target.value)}
-            className="p-2 border rounded-lg"
+            className='p-2 border rounded-lg'
           >
-            <option value="">Pick value</option>
+            <option value=''>Pick value</option>
             {plants.map((item) => (
               <option key={item._id} value={item._id}>
                 {item.species}
               </option>
             ))}
           </select>
-          <p className="-mb-1 font-medium">Plant cuttings available</p>
+          <div
+            className=' drag mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10'
+            id='drop_zone'
+            onDrop={(ev) => dropHandler(ev)}
+            onDragOver={(ev) => dragOverHandler(ev)}
+          >
+            <div className='text-center'>
+              {plantPicture ? (
+                <div>
+                  <img
+                    className='mx-auto'
+                    alt='not found'
+                    width={"150px"}
+                    src={
+                      typeof plantPicture === "string"
+                        ? plantPicture
+                        : URL.createObjectURL(plantPicture)
+                    }
+                  />
+                  <button
+                    type='button'
+                    className='text-red-500 text-xs'
+                    onClick={() => setPlantPicture(null)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              ) : (
+                <svg
+                  className='mx-auto h-12 w-12 text-gray-300'
+                  viewBox='0 0 24 24'
+                  fill='currentColor'
+                  aria-hidden='true'
+                >
+                  <path
+                    fillRule='evenodd'
+                    d='M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z'
+                    clipRule='evenodd'
+                  />
+                </svg>
+              )}
+              <div className='mt-4 flex text-sm leading-6 text-gray-600'>
+                <label
+                  htmlFor='image'
+                  className='relative cursor-pointer rounded-md font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500'
+                >
+                  <span className='text-emerald-600'>Upload a file</span>
+
+                  <input
+                    className='sr-only'
+                    id='image'
+                    name='image'
+                    type='file'
+                    value=''
+                    onChange={(event) => {
+                      setPlantPicture(event.target.files[0]);
+                    }}
+                  />
+                </label>
+                <p className='pl-1'>or drag and drop</p>
+              </div>
+              <p className='text-xs leading-5 text-gray-600'>
+                PNG, JPG, GIF up to 10MB
+              </p>
+            </div>
+          </div>
+          <p className='-mb-1 font-medium'>Plant cuttings available</p>
           <input
-            type="number"
+            type='number'
             value={plantCutting}
             onChange={(event) => setPlantCutting(event.target.value)}
             required
-            placeholder="Plant cuttings available"
-            className="p-2 border rounded-lg"
+            placeholder='Plant cuttings available'
+            className='p-2 border rounded-lg'
           />
-          <p className="-mb-1 font-medium">Plant size</p>
+          <p className='-mb-1 font-medium'>Plant size</p>
           <input
-            type="text"
+            type='text'
             value={plantSize}
             onChange={(event) => setPlantSize(event.target.value)}
             required
-            placeholder="Plant size"
-            className="p-2 border rounded-lg"
+            placeholder='Plant size'
+            className='p-2 border rounded-lg'
           />
 
-          <label className="flex items-center">
+          <label className='flex items-center'>
             <input
-              type="checkbox"
+              type='checkbox'
               defaultChecked
               onChange={(event) => setReminderSettings(event.target.checked)}
-              className="mr-2"
+              className='mr-2'
             />
-            <p className="text-xs text-gray-400">
+            <p className='text-xs text-gray-400'>
               I want to receive plant care reminders
             </p>
           </label>
           <button
-            type="submit"
+            type='submit'
             onClick={handleSubmit}
-            className="bg-emerald-600 text-white p-2 rounded-full self-center mt-4"
+            className='bg-emerald-600 text-white p-2 rounded-full self-center mt-4'
           >
             Update Plant
           </button>
